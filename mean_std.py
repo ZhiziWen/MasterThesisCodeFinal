83c9423e2ea42572074a25914808438802f2aa17
ZhiziWen <wenzhizi@foxmail.com>
2024-04-01 00:44:09 +0200

    add comments

--- mean_std.py
+++ mean_std.py
@@ -1,15 +1,20 @@
+'''
+This file calculates the mean and standard deviation of the metrics in the metrics report file.
+'''
+
 import pandas as pd
+from config import timestr, general_output_folder
 
 # Load the dataset
-file_path_bpic2012 = 'D:/SS2023/MasterThesis/code/metrics_report/report_Sepsis 2_20240316-1257.xlsx'
-df_bpic2012 = pd.read_excel(file_path_bpic2012, sheet_name='Original Data')
+file_path = general_output_folder + 'metrics_report/report_Sepsis 1_20240331-2356.xlsx'
+df = pd.read_excel(file_path, sheet_name='Original Data')
+dataset_name = "Sepsis 1" # options: Sepsis 1, Sepsis 2, BPIC2012
 
-# Ensure 'Method' retains its original order by converting it to a categorical type
-# with categories set in the order they appear
-df_bpic2012['Method'] = pd.Categorical(df_bpic2012['Method'], categories=pd.unique(df_bpic2012['Method']), ordered=True)
+# Ensure 'Method' retains its original order
+df['Method'] = pd.Categorical(df['Method'], categories=pd.unique(df['Method']), ordered=True)
 
 # Exclude 'Trial' and 'Support' from calculations
-df_filtered = df_bpic2012.drop(columns=['Trial', 'Support'])
+df_filtered = df.drop(columns=['Trial', 'Support', 'Accuracy'])
 
 # Calculate mean and standard deviation
 grouped_mean = df_filtered.groupby(['Method', 'Label']).mean()
@@ -29,9 +34,7 @@ for col in grouped_mean.columns:
 # Reorder the DataFrame according to the original order of appearance of methods
 combined_stats_adjacent = combined_stats_adjacent.reindex(columns=columns_ordered)
 
-# Save to a new Excel sheet, maintaining the order of 'Method'
-output_path_bpic2012_adjusted = 'D:/SS2023/MasterThesis/code/metrics_report/combined_stats_Sepsis2_adjusted.xlsx'
+# Save to a new Excel sheet
+output_path_bpic2012_adjusted = general_output_folder + f'metrics_report/combined_stats_{dataset_name}.xlsx'
 with pd.ExcelWriter(output_path_bpic2012_adjusted, engine='openpyxl') as writer:
     combined_stats_adjacent.to_excel(writer, sheet_name='Sheet1')
-
-output_path_bpic2012_adjusted
