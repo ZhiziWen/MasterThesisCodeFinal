161f25e679c4e03f1d62c44383d2bd82b9db0347
ZhiziWen <wenzhizi@foxmail.com>
2024-04-01 00:19:08 +0200

    optimize import

--- Preprocess_dataframe.py
+++ Preprocess_dataframe.py
@@ -1,25 +1,5 @@
-import numpy as np
-import cv2
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-import matplotlib.pyplot as plt
 import pandas as pd
-import random
-import time
-
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from xgboost import XGBClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.preprocessing import LabelEncoder, StandardScaler, MultiLabelBinarizer, OneHotEncoder, MinMaxScaler
-from sklearn.metrics import classification_report
-
-from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
-from imblearn.under_sampling import RandomUnderSampler, NearMiss
-
-from LSTMencoder_pytorch import LSTM, SequenceDataset
-from resampling_and_classification import resampling_techniques
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from AggregateTransformer import AggregateTransformer
 from StaticTransformer import StaticTransformer
 
