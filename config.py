f19876ccac33bfbbf934eb54aba95fd8c05a91d8
ZhiziWen <wenzhizi@foxmail.com>
2024-03-31 23:59:29 +0200

    add global variable general_output_folder

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ This file contains the global variables for the project.
 '''
 
 
-from datetime import datetime
 import time
 
 timestr = time.strftime("%Y%m%d-%H%M")
-general_output_folder = "D:/SS2023/MasterThesis/codeFINAL/code_final/"
+general_output_folder = "D:/SS2023/MasterThesis/codeFINAL/code_final/" # change this to the path where you want to save the output files
