89aa4f08e3fac239ad544f060adc5c7f4fddf51b
ZhiziWen <wenzhizi@foxmail.com>
2024-04-06 12:41:43 +0200

    add warning if not change project file name

--- global_variables.py
+++ global_variables.py
@@ -6,4 +6,7 @@ This file contains the global variables for the project.
 import time
 
 timestr = time.strftime("%Y%m%d-%H%M")
-project_folder = "D:/SS2023/MasterThesis/codeFINAL/code_final/" # change this to the path where the project folder is located
+project_folder = "D:/SS2023/MasterThesis/codeFINAL/code_final/" # change this to the path where the project folder is located, example: D:/SS2023/MasterThesis/codeFINAL/code_final/
+
+if project_folder == "":
+    raise ValueError("Please specify the project folder path in the global_variables.py file.")
